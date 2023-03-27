27ee8509821e2225b911193540f579096740bf17
danriav <daniel_rincon5@hotmail.com>
2023-03-27 11:48:16 -0600

    DBConnections changed

--- db/Connection.go
+++ db/Connection.go
@@ -1,20 +1,18 @@
 package connection
 
-
 import (
 	"database/sql"
 )
 
-
-func DBConnection()(db *sql.DB){
+func DBConnection() (db *sql.DB) {
 	Driver := "mysql"
 	User := "root"
 	Password := ""
 	Name := "aires_acondicionados"
 
-	db, err := sql.Open(Driver, User+":"+Password+"@tcp(127.0.0.1:3307)/"+Name)
+	db, err := sql.Open(Driver, User+":"+Password+"@tcp()/"+Name)
 	if err != nil {
-		panic (err.Error())
+		panic(err.Error())
 	}
 	return db
 }
